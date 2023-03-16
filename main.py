8a0232dabe89ab11a161e4e392afe7c5a8fd57d2
Andrew Trench <andrewjohntrench@gmail.com>
2023-03-16 14:22:01 +0200

    fixing st.text

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ def cal_attribute_score(ratio, ads_score, ssl, whois):
 
 
 st.title("TrustList URL Score Calculator")
-st.text("Enter the following attributes to get the URL Attribute score")
+#st.text("Enter the following attributes to get the URL Attribute score")
 
-st.text("Ratio is between 0 and 1500. Eg New York Times is around 1200 with 6bn links and 350m domains")
+#st.text("Ratio is between 0 and 1500. Eg New York Times is around 1200 with 6bn links and 350m domains")
 ratio = st.number_input("Ratio", min_value=0.0, max_value=1500.0, step=0.1)
 
-st.text("Ads Score is between 0 and 1 ie. True or False")
+#st.text("Ads Score is between 0 and 1 ie. True or False")
 ads_score = st.number_input("Ads Score", min_value=0.0, max_value=1.0, step=1.0)
 
-st.text("Whois is between 0, 0.5 and 1 ie. 0 if no record, 0.5 if date < 1 year, 1 if date > 1 year")
+#st.text("Whois is between 0, 0.5 and 1 ie. 0 if no record, 0.5 if date < 1 year, 1 if date > 1 year")
 whois = st.number_input("Whois", min_value=0.0, max_value=1.0, step=0.5)
 
-st.text("SSL is between 0 and 1 ie. True or False")
+#st.text("SSL is between 0 and 1 ie. True or False")
 ssl = st.number_input("SSL", min_value=0.0, max_value=1.0, step=1.0)
 
 score = cal_attribute_score(ratio, ads_score, ssl, whois)
@@ -30,30 +30,30 @@ if reset:
     ratio, whois, ssl, ads_score = 0.0, 0.0, 0.0, 0.0
     show_score.write(f"Score: {score}")
 
-st.text("Enter the following attributes to get the URL Quality score")
+#st.text("Enter the following attributes to get the URL Quality score")
 
-st.text("Contact is either 0 and 1 ie. True or False")
+#st.text("Contact is either 0 and 1 ie. True or False")
 contact = st.number_input("Contact", min_value=0.0, max_value=1.0, step=1.0)
 
-st.text("Contact Confidence is between 0 and 1 eg. 0.3")
+#st.text("Contact Confidence is between 0 and 1 eg. 0.3")
 contact_confidence = st.number_input("Contact Confidence", min_value=0.0, max_value=1.0, step=0.1)
 
-st.text("Policy is either 0 and 1 ie. True or False")
+#st.text("Policy is either 0 and 1 ie. True or False")
 policy = st.number_input("Policy", min_value=0.0, max_value=1.0, step=1.0)
 
-st.text("Policy Confidence is between 0 and 1 eg. 0.3")
+#st.text("Policy Confidence is between 0 and 1 eg. 0.3")
 policy_confidence = st.number_input("Policy Confidence", min_value=0.0, max_value=1.0, step=0.1)
 
-st.text("Authors is either 0 and 1 ie. True or False")
+#st.text("Authors is either 0 and 1 ie. True or False")
 authors = st.number_input("Authors", min_value=0.0, max_value=1.0, step=1.0)
 
-st.text("Authors Confidence is between 0 and 1 eg. 0.3")
+#st.text("Authors Confidence is between 0 and 1 eg. 0.3")
 authors_confidence = st.number_input("Authors Confidence", min_value=0.0, max_value=1.0, step=0.1)
 
-st.text("Ad Indicator is either 0 and 1 ie. True or False")
+#st.text("Ad Indicator is either 0 and 1 ie. True or False")
 ad_indicator = st.number_input("Ad Indicator", min_value=0.0, max_value=1.0, step=1.0)
 
-st.text("Ad Indicator Confidence is between 0 and 1 eg. 0.3")
+#st.text("Ad Indicator Confidence is between 0 and 1 eg. 0.3")
 ad_indicator_confidence = st.number_input("Ad Indicator Confidence", min_value=0.0, max_value=1.0, step=0.1)
 
 
